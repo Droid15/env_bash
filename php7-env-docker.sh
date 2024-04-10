c936c2374170c011c037ac460e551e709f0240ea
Droid15 <lyr1234@126.com>
2024-04-10 16:23:49 +0800

    Update php7-env-docker.sh
    fix

--- php7-env-docker.sh
+++ php7-env-docker.sh
@@ -6,5 +6,5 @@
 #sudo yum install docker
 
 WEB_PORT=8888
-IMG_TAG=myphp72
+IMG_TAG=179633571/php72w
 sudo docker run --name=myphp72w -it -d -p $WEB_PORT:8080 $IMG_TAG "/bin/bash" && sudo docker exec -it myphp72w "/webon.sh"
