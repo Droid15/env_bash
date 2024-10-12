576d795582bb30d106714323524e8bb07d7d9dd8
Droid15 <lyr1234@126.com>
2024-10-12 11:08:48 +0800

    Update node14.sh

--- node14.sh
+++ node14.sh
@@ -2,14 +2,14 @@
 
 yum install wget
 
-wget https://npm.taobao.org/mirrors/node/v14.3.0/node-v14.3.0-linux-x64.tar.xz
+wget https://nodejs.org/dist/v14.21.3/node-v14.21.3-linux-x64.tar.xz
 
-xz -d node-v14.3.0-linux-x64.tar.xz
+xz -d node-v14.21.3-linux-x64.tar.xz
 
-tar -xvf node-v14.3.0-linux-x64.tar
+tar -xvf node-v14.21.3-linux-x64.tar
 
-mv node-v14.3.0-linux-x64 /usr/local/node-v14.3.0
+mv node-v14.21.3-linux-x64 /usr/local/node-v14.21
 
-ln -s /usr/local/node-v14.3.0/bin/npm /usr/local/bin/npm
-ln -s /usr/local/node-v14.3.0/bin/node /usr/local/bin/node
+ln -s /usr/local/node-v14.21/bin/npm /usr/local/bin/npm
+ln -s /usr/local/node-v14.21/bin/node /usr/local/bin/node
 
